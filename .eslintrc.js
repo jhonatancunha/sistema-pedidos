dfc4b7144d3d06f1fd69634398713eef40a6c19a
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-07-07 23:27:13 -0300

    :tada: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {extensions: ['.js', '.jsx']}
+    ],
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-single']
+  }
+};
